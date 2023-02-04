9d294d57b70a853dcf07a7a2b3420f0513f4cf5a
igor_nebosenko <igor.nebosenko@gmail.com>
2023-02-04 04:04:23 +0200

    - Add LevelSelectView - Add signatures of scripts - Disable LunarConsole

---
+++ Assets/Scripts/UI/Menu/Presenters/LevelSelectPresenter.cs
@@ -0,0 +1,26 @@
+﻿using CBH.Core;
+using CBH.UI.Menu.Views;
+using ElectrumGames.MVP;
+using ElectrumGames.MVP.Managers;
+
+namespace CBH.UI.Menu.Presenters
+{
+    public class LevelSelectPresenter : Presenter<LevelSelectView>
+    {
+        private ViewManager _viewManager;
+        private GameData _gameData;
+        
+        public LevelSelectPresenter(ViewManager viewManager, GameData gameData, LevelSelectView view) : base(view)
+        {
+            _viewManager = viewManager;
+            _gameData = gameData;
+            
+            view.Init(this);
+        }
+
+        public void OnToMenuClicked()
+        {
+            _viewManager.ShowView<MainMenuPresenter>();
+        }
+    }
+}
---
+++ Assets/Scripts/UI/Menu/Views/LevelSelectView.cs
@@ -0,0 +1,22 @@
+﻿using CBH.UI.Menu.Presenters;
+using ElectrumGames.MVP;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace CBH.UI.Menu.Views
+{
+    public class LevelSelectView : View<LevelSelectPresenter>
+    {
+        [SerializeField] private Button toMenuButton;
+
+        public void Init(LevelSelectPresenter presenter)
+        {
+            toMenuButton.onClick.AddListener(presenter.OnToMenuClicked);
+        }
+
+        private void OnDestroy()
+        {
+            toMenuButton.onClick.RemoveListener(Presenter.OnToMenuClicked);
+        }
+    }
+}
