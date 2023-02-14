0ecf6c504544f9df9742b22bd5b15bbe0776b7fe
igor_nebosenko <igor.nebosenko@gmail.com>
2023-02-14 16:19:22 +0200

    - Added boost button & script for it

---
+++ Assets/Scripts/UI/Game/Compnents/BoostButton.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem.Layouts;
+using UnityEngine.InputSystem.OnScreen;
+using UnityEngine.UI;
+
+namespace CBH.UI.Game
+{
+    public class BoostButton : OnScreenControl, IPointerDownHandler, IPointerUpHandler
+    {
+        [SerializeField] private Image buttonImage;
+        [InputControl(layout = "Button")] 
+        [SerializeField] private string controlPath;
+
+        private static readonly Color32 ButtonNotPressedColor = new Color32(255, 255, 255, 127);
+
+        private bool _isPressed;
+
+        protected override string controlPathInternal
+        {
+            get => controlPath;
+            set => controlPath = value;
+        }
+
+        private void Update()
+        {
+            SendValueToControl(_isPressed ? 1.0f : 0f);
+        }
+
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            _isPressed = true;
+            buttonImage.color = Color.white;
+        }
+
+        public void OnPointerUp(PointerEventData eventData)
+        {
+            _isPressed = false;
+            buttonImage.color = ButtonNotPressedColor;
+        }
+    }
+}
