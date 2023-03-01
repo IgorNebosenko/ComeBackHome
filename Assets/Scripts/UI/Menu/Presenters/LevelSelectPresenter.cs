4f4e41a53e30c940df3df23a0851c2529f3c88ab
igor_nebosenko <igor.nebosenko@gmail.com>
2023-03-01 02:06:06 +0200

    - Add level select events

--- Assets/Scripts/UI/Menu/Presenters/LevelSelectPresenter.cs
+++ Assets/Scripts/UI/Menu/Presenters/LevelSelectPresenter.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections;
+using CBH.Analytics;
+using CBH.Analytics.Events;
 using CBH.Core;
 using CBH.UI.Menu.Views;
 using ElectrumGames.MVP;
@@ -10,6 +12,7 @@ namespace CBH.UI.Menu.Presenters
 {
     public class LevelSelectPresenter : Presenter<LevelSelectView>
     {
+        private IAnalyticsManager _analyticsManager;
         private ViewManager _viewManager;
         private GameData _gameData;
 
@@ -17,20 +20,24 @@ namespace CBH.UI.Menu.Presenters
 
         public int LastCompletedScene => _gameData.LastCompletedScene;
         
-        public LevelSelectPresenter(ViewManager viewManager, GameData gameData, LevelSelectView view) : base(view)
+        public LevelSelectPresenter(ViewManager viewManager, IAnalyticsManager analyticsManager,
+            GameData gameData, LevelSelectView view) : base(view)
         {
+            _analyticsManager = analyticsManager;
             _viewManager = viewManager;
             _gameData = gameData;
         }
 
         public void OnToMenuClicked()
         {
+            _analyticsManager.SendEvent(new CloseLevelSelectMenuEvent(_gameData.LastCompletedScene));
             _viewManager.ShowView<MainMenuPresenter>();
         }
 
         public void LoadLevel(int level)
         {
             _cachedSelectedLevel = level;
+            _analyticsManager.SendEvent(new StartLevelFromMenuEvent(level, _gameData.LastCompletedScene));
             Observable.FromCoroutine(LoadLevelProcess).Subscribe();
         }
 
