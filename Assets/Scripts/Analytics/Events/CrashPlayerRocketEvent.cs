0414a7591198ad65153e329b978e6dcdd85fbc14
igor_nebosenko <igor.nebosenko@gmail.com>
2023-03-07 05:26:09 +0200

    - Return events to dev branch

--- Assets/Scripts/Analytics/Events/CrashPlayerRocketEvent.cs
+++ Assets/Scripts/Analytics/Events/CrashPlayerRocketEvent.cs
@@ -4,19 +4,17 @@
     {
         public override string Key => "crash_player_rocket";
 
-        public CrashPlayerRocketEvent(int levelId, int maxOpenedLevel, float flyTime, int countBoostButtonPressed,
-            int countRotateLeftButtonPressed, int countRotateRightButtonPressed, float boostTime,
-            float rotationLeftTime, float rotationRightTime, int countLandingOnLandingPad)
+        public CrashPlayerRocketEvent(int levelId, int maxOpenedLevel, float flyTime, InputData inputData, int countLandingOnLandingPad)
         {
             Data.Add("level_id", levelId);
             Data.Add("max_opened_level", maxOpenedLevel);
             Data.Add("fly_time", flyTime);
-            Data.Add("count_boost_button_pressed", countBoostButtonPressed);
-            Data.Add("count_rotate_left_button_pressed", countRotateLeftButtonPressed);
-            Data.Add("count_rotate_right_button_pressed", countRotateRightButtonPressed);
-            Data.Add("boost_time", boostTime);
-            Data.Add("rotation_left_time", rotationLeftTime);
-            Data.Add("rotation_right_time", rotationRightTime);
+            Data.Add("count_boost_button_pressed", inputData.countBoostPressed);
+            Data.Add("count_rotate_left_button_pressed", inputData.countRotationLeftPressed);
+            Data.Add("count_rotate_right_button_pressed", inputData.countRotationRightPressed);
+            Data.Add("boost_time", inputData.timeBoostPressed);
+            Data.Add("rotation_left_time", inputData.timeRotationLeftPressed);
+            Data.Add("rotation_right_time", inputData.timeRotationRightPressed);
             Data.Add("count_landing_on_landing_pad", countLandingOnLandingPad);
         }
     }
--- Assets/Scripts/Core/GameManager.cs
+++ Assets/Scripts/Core/GameManager.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections;
 using CBH.Ads;
+using CBH.Analytics;
+using CBH.Analytics.Events;
 using CBH.Core.Audio;
 using CBH.Core.Entity;
 using CBH.Core.IAP;
@@ -15,11 +17,18 @@ namespace CBH.Core
         private GameData _gameData;
         private AdsData _adsData;
         private AdsConfig _adsConfig;
+        private InputData _inputData;
         private IAdsProvider _adsProvider;
         private IStorePurchaseController _storePurchaseController;
-
+        private IAnalyticsManager _analyticsManager;
+        
         private bool _isLanded;
         private bool _isGameEnded;
+
+        private float _timeAtStartLevel;
+        
+        private int _attemptsLanding;
+        private float _timeStayLeft = LandingDuration;
 
         private const float RestartDuration = 3f;
         private const float RestartTickDuration = 1f;
@@ -38,19 +47,21 @@ namespace CBH.Core
         public event Action<TimeSpan> UpdateFlyTime; 
 
         public RocketState CurrentState { get; private set; }
-
         public TimeSpan TimeFly { get; private set; } = TimeSpan.Zero;
 
-        public GameManager(GameData gameData, AdsData adsData, AdsConfig adsConfig,
-            IAdsProvider adsProvider, IStorePurchaseController storePurchaseController)
+        public GameManager(GameData gameData, AdsData adsData, AdsConfig adsConfig, InputData inputData, IAdsProvider adsProvider, 
+            IStorePurchaseController storePurchaseController, IAnalyticsManager analyticsManager)
         {
             _gameData = gameData;
             _adsData = adsData;
             _adsConfig = adsConfig;
+            _inputData = inputData;
             _adsProvider = adsProvider;
             _storePurchaseController = storePurchaseController;
+            _analyticsManager = analyticsManager;
 
             CurrentState = RocketState.Live;
+            _timeAtStartLevel = Time.realtimeSinceStartup;
         }
 
         public void HandleRocketState(RocketState state)
@@ -61,13 +72,20 @@ namespace CBH.Core
             {
                 case RocketState.Dead:
                     AudioHandler.StopLoopSound();
+                    _analyticsManager.SendEvent(new CrashPlayerRocketEvent(SceneManager.GetActiveScene().buildIndex,
+                        _gameData.LastCompletedScene,
+                        (float) TimeFly.TotalSeconds, _inputData, _attemptsLanding));
                     LevelLose?.Invoke();
                     Observable.FromCoroutine(RestartProcess).Subscribe();
                     break;
                 case RocketState.LandFinishPad:
+                    _analyticsManager.SendEvent(new LandingOnLandingPadEvent(SceneManager.GetActiveScene().buildIndex,
+                        _gameData.LastCompletedScene, (float) TimeFly.TotalSeconds, ++_attemptsLanding));
                     Observable.FromCoroutine(LandingProcess).Subscribe();
                     break;
                 case RocketState.LeaveFinishPad:
+                    _analyticsManager.SendEvent(new LeaveLandingPadEvent(SceneManager.GetActiveScene().buildIndex,
+                        _gameData.LastCompletedScene, (float) TimeFly.TotalSeconds, _attemptsLanding, _timeStayLeft));
                     PlatformLeave?.Invoke();
                     CurrentState = RocketState.Live;
                     break;
@@ -76,6 +94,9 @@ namespace CBH.Core
 
         public void StartFlyProcess()
         {
+            var timeDifference = Time.realtimeSinceStartup - _timeAtStartLevel;
+            _analyticsManager.SendEvent(new LaunchFromLaunchPadEvent(SceneManager.GetActiveScene().buildIndex, 
+                _gameData.LastCompletedScene, timeDifference));
             Observable.FromCoroutine(UpdateFlyTimeProcess).Subscribe();
         }
 
@@ -91,6 +112,8 @@ namespace CBH.Core
                 timeLeft -= RestartTickDuration;
             }
 
+            var isNeedAd = false;
+            
             if (!_storePurchaseController.HasNoAdsSubscription)
             {
                 _adsData.countRestartsFromLastAd++;
@@ -99,10 +122,15 @@ namespace CBH.Core
                 if (_adsData.countRestartsFromLastAd >= _adsConfig.countRestartsBetweenAds ||
                     _adsData.timeFlyFromLastAd >= _adsConfig.timeFlyBetweenAds)
                 {
+                    isNeedAd = true;
                     _adsProvider.ShowInterstitial();
                     _adsData.Reset();
                 }
             }
+
+            _analyticsManager.SendEvent(new RestartLevelEvent(SceneManager.GetActiveScene().buildIndex,
+                _gameData.LastCompletedScene, _adsData.timeFlyFromLastAd, _adsData.countRestartsFromLastAd, isNeedAd,
+                _storePurchaseController.HasNoAdsSubscription));
 
             var currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
             yield return SceneManager.LoadSceneAsync(currentSceneIndex);
@@ -110,21 +138,27 @@ namespace CBH.Core
         
         private IEnumerator LandingProcess()
         {
+            _timeStayLeft = LandingDuration;
             _isLanded = true;
-            var timeLeft = LandingDuration;
 
-            while (timeLeft >= 0 && CurrentState == RocketState.LandFinishPad)
+            while (_timeStayLeft >= 0 && CurrentState == RocketState.LandFinishPad)
             {
-                timeLeft -= Time.deltaTime;
-                PlatformStay?.Invoke(timeLeft);
+                _timeStayLeft -= Time.deltaTime;
+                PlatformStay?.Invoke(_timeStayLeft);
                 yield return new WaitForEndOfFrame();
             }
 
             if (CurrentState == RocketState.LandFinishPad)
             {
                 _isGameEnded = true;
+                _analyticsManager.SendEvent(new LoadNextGameLevelEvent(SceneManager.GetActiveScene().buildIndex,
+                    _gameData.LastCompletedScene, _adsData.timeFlyFromLastAd, _adsData.countRestartsFromLastAd));
                 BeforeWin?.Invoke();
                 yield return new WaitForSeconds(BeforeWinDuration);
+                
+                _analyticsManager.SendEvent(new SuccessfulLandingRocketEvent(SceneManager.GetActiveScene().buildIndex,
+                    _gameData.LastCompletedScene,
+                    (float) TimeFly.TotalSeconds, _inputData, _attemptsLanding));
 
                 LevelWin?.Invoke();
                 var nextScene = SceneManager.GetActiveScene().buildIndex + 1;
@@ -149,7 +183,7 @@ namespace CBH.Core
                 if (_isLanded)
                     continue;
                 
-                TimeFly += TimeSpan.FromSeconds(Time.deltaTime);
+                TimeFly = TimeFly.Add(TimeSpan.FromSeconds(Time.deltaTime));
                 UpdateFlyTime?.Invoke(TimeFly);
             }
         }
