6b15b50ef281aed1b8d0a0cf901cc670cdb91c94
igor_nebosenko <igor.nebosenko@gmail.com>
2023-04-17 06:08:09 +0300

    - Create tutoriak installer w\o logic

---
+++ Assets/Scripts/Injection/TutorialInstaller.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using CBH.Core.Collision;
+using CBH.Core.Entity.Input;
+using CBH.UI.Game.UI.Game;
+using UnityEngine;
+
+namespace CBH.Injection
+{
+    public class TutorialInstaller : BaseSceneInstaller
+    {
+        [SerializeField] private RocketController rocketController;
+        [SerializeField] private FinishCollisionObject landingPad;
+
+        protected override Assembly UiAssembly => typeof(GameAssemblyPlaceholder).Assembly;
+
+        public override void InstallBindings()
+        {
+            base.InstallBindings();
+        }
+    }
+}
