72fa53855248a1c5d9c0ad254f69ef06fad0ddc8
igor_nebosenko <igor.nebosenko@gmail.com>
2023-04-18 00:02:08 +0300

    - Add TutorialInitialBoostPopup signature

---
+++ Assets/Scripts/UI/Game/Presenters/TutorialInitialBoostPopupPresenter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using CBH.UI.Game.Views;
+using ElectrumGames.MVP;
+using ElectrumGames.MVP.Utils;
+
+namespace CBH.UI.Game.Presenters
+{
+    public class TutorialInitialBoostPopupPresenter : PopupPresenterCoroutine<TutorialInitialBoostPopup, PopupArgs, PopupResult>
+    {
+        public TutorialInitialBoostPopupPresenter(TutorialInitialBoostPopup view) : base(view)
+        {
+        }
+
+        public override IEnumerable<PopupResult> Init(PopupArgs args)
+        {
+            yield return null;
+        }
+
+        public void OnFadeClicked()
+        {
+            Close();
+        }
+    }
+}
---
+++ Assets/Scripts/UI/Game/Views/TutorialInitialBoostPopup.cs
@@ -0,0 +1,24 @@
+﻿using CBH.UI.Game.Presenters;
+using ElectrumGames.MVP;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace CBH.UI.Game.Views
+{
+    public class TutorialInitialBoostPopup : View<TutorialInitialBoostPopupPresenter>
+    {
+        [SerializeField] private Button fadeBackground;
+        [SerializeField] private Rect pointer;
+
+        private void Start()
+        {
+            fadeBackground.onClick.AddListener(Presenter.OnFadeClicked);
+        }
+
+        private void OnDestroy()
+        {
+            fadeBackground.onClick.RemoveListener(Presenter.OnFadeClicked);
+        }
+
+    }
+}
